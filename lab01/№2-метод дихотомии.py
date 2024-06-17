7780f48b3abc5026640a31b121dc7f3ae4f946b6
Anastasia-zinchenko <147321900+Anastasia-zinchenko@users.noreply.github.com>
2024-06-17 22:07:47 +0300

    Update №2-метод дихотомии.py

--- lab01/№2-метод дихотомии.py
+++ lab01/№2-метод дихотомии.py
@@ -22,38 +22,3 @@ eps = 1e-6
 root = bisection_method(a, b, eps)
 if root:
     print("Корень уравнения: ", root)
-№ 2 (метод итераций С++)
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-
-double f(double x) {
-    return log(x) + pow(x + 1, 3);
-}
-
-double df(double x) {
-    return 1 / x + 3 * pow(x + 1, 2);
-}
-
-double iterate(double x0, double epsilon) {
-    double x = x0;
-    while (true) {
-        double x_next = x - f(x) / df(x);
-        if (abs(x_next - x) < epsilon) {
-            return x_next;
-        }
-        x = x_next;
-    }
-}
-
-int main() {
-    double x0 = 0.5;  // Начальное приближение
-    double epsilon = 1e-6;
-    
-    double root = iterate(x0, epsilon);
-    
-    cout << "Корень уравнения: x = " << root << endl;
-    
-    return 0;
-}
